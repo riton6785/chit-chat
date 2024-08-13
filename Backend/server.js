30d85570eb07702c3631fef43f71e0abda5d8f67
adsh-odoo <adsh@odoo.com>
2024-08-13 21:36:46 +0530

    Integrated the socket.io for sending and recieving the message and aslo the typic indicator functionality

--- Backend/server.js
+++ Backend/server.js
@@ -30,4 +30,43 @@ app.get("/api/chats", (req, res)=>{
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(process.env.PORT, console.log(`server is running on port ${process.env.PORT }`))
+const server = app.listen(process.env.PORT, console.log(`server is running on port ${process.env.PORT }`))
+
+const io = require('socket.io')(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: "http://localhost:5173"
+    },
+})
+
+io.on("connection", (socket)=>{
+    console.log("connected to socket .io", socket.id)
+    socket.on("setup", (userData)=> {
+        socket.join(userData._id)
+        console.log(userData);
+        socket.emit("connected");
+    })
+
+    socket.on("join-room", (room)=> {
+        socket.join(room);
+        console.log("user jooined room", room);
+    })
+
+    socket.on("typing", (room)=> socket.in(room).emit("typing"));
+    socket.on("stop typing", (room)=> socket.in(room).emit("stop typing"));
+
+    socket.on("new-message", (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat;
+        if(!chat.users) return;
+
+        chat.users.forEach((user) => {
+            if(user._id == newMessageRecieved.sender._id) return;
+            socket.in(user._id).emit("message-recieved", newMessageRecieved);
+        })
+    })
+
+    socket.off("setup", ()=> {
+        console.log("user Diconnedcted");
+        socket.leave(userData._id);
+    })
+})
